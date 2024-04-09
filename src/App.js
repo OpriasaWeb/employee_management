250d489a3d72429921a062b9215b61dfdfdd4fa6
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-04-09 22:59:34 +0800

    feat: imported components and routed to UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { BrowserRouter, Routes, Route }  from "react-router-dom"
+import SignUp from "./components/signup"
+import Login from "./components/login"
+import Dashboard from './components/dashboard';
+import Home from './components/home';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello mcWorld!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+
+          <Route path='/' element={<Dashboard />}>
+            <Route path='' element={<Home />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>  
     </div>
   );
 }
